6fa3e696bbe8991e7393255db414898e6380c25f
abdulkerimatik <abdulkerimatik@gmail.com>
2026-06-11 08:49:44 +0300

    bump version to 1.4.8 and 1.1.8 in build.gradle.kts and package.json

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import proguard.gradle.ProGuardTask
 
 group = "ai.blamely"
-version = "1.4.7"
+version = "1.4.8"
 
 java {
     toolchain { languageVersion.set(org.gradle.jvm.toolchain.JavaLanguageVersion.of(17)) }
