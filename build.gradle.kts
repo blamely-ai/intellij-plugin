1ae3c980098f2f2ace70433c493b972fffa1489e
blamely-ai <admin@blamely.ai>
2026-06-16 03:06:16 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import proguard.gradle.ProGuardTask
 
 group = "ai.blamely"
-version = "1.6.0"
+version = "1.6.1"
 
 java {
     toolchain { languageVersion.set(org.gradle.jvm.toolchain.JavaLanguageVersion.of(17)) }
